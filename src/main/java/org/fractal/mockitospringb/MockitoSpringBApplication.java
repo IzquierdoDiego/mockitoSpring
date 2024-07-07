40dd4ee57f9d4db33d04d04257916086810ae256
Diego Izquierdo <izquierdodiegof@gmail.com>
2024-07-07 11:52:59 -0500

    initial commit

---
+++ src/main/java/org/fractal/mockitospringb/MockitoSpringBApplication.java
@@ -0,0 +1,13 @@
+package org.fractal.mockitospringb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoSpringBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockitoSpringBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fractal/mockitospringb/MockitoSpringBApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fractal.mockitospringb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoSpringBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
